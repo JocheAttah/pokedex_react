6bba701fb0298836657697f30783fca48067af51
Attah David <jocheattahdavid@gmail.com>
2020-04-24 00:21:43 +0100

    added Pokegame Component

--- pokedex-react/src/App.js
+++ pokedex-react/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
+// import Pokedex from './Pokedex';
 
 function App() {
   return (
     <div className="App">
-      <Pokedex />
+      <Pokegame />
       
     </div>
   );
--- pokedex-react/src/Pokecard.js
+++ pokedex-react/src/Pokecard.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import './Pokecard.css'
 
-const poke_api =`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`
+// const poke_api =`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`
+const poke_api =`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/`
+
+let padToThree = (num)=> (num <= 999 ? `00${num}`.slice(-3) : num);
 
 
 class Pokecard extends Component {
     render(){
-        let img = `${poke_api}${this.props.id}.png`
+        let img = `${poke_api}${padToThree(this.props.id)}.png`
         return (
             <div className="Pokecard">
                 <h1 className="Pokecard-title" >{this.props.name}</h1>
