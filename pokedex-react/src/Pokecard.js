dc573ad4ace0a11853f63e69d29db4ed804e65de
Attah David <jocheattahdavid@gmail.com>
2020-04-27 12:20:25 +0100

    styled the UI

--- pokedex-react/src/Pokecard.js
+++ pokedex-react/src/Pokecard.js
@@ -9,11 +9,11 @@ let padToThree = (num)=> (num <= 999 ? `00${num}`.slice(-3) : num);
 
 class Pokecard extends Component {
     render(){
-        let img = `${poke_api}${padToThree(this.props.id)}.png`
+        let img = `${poke_api}${padToThree (this.props.id)}.png`
         return (
             <div className="Pokecard">
                 <h1 className="Pokecard-title" >{this.props.name}</h1>
-                <img src={img} alt={this.props.name} />
+                <div className="Pokecard-image"><img src={img} alt={this.props.name} /></div>
                 <div className="Pokecard-data">Type: {this.props.type}</div>
                 <div className="Pokecard-data">Exp: {this.props.exp}</div>
             </div>
